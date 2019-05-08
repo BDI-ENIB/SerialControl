9f46268c734898a49762504325d256d00f8d99be
Steins <jce.lepage@gmail.com>
2019-05-08 19:02:36 +0200

    Test making
    * tried several communication tests with the updated linux code

--- arduino/SerialControl/SerialControl.ino
+++ arduino/SerialControl/SerialControl.ino
@@ -1,18 +1,24 @@
-int led = 13;
+int led = LED_BUILTIN;
 int i = 0;
+bool spam = false; 
 
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(led, OUTPUT);
-  Serial.begin(9600);     
+  Serial.begin(9600);    
 }
 
 void loop() {
+  digitalWrite(led,LOW);
   if (Serial.available() > 0) {
     // read the incoming byte:
     String message = Serial.readStringUntil(';');
     if(message.equals("whois")) {
-      Serial.println("I'm Groot;");
+      spam = true;
+      Serial.println("I'm_Jesus;");
+      digitalWrite(led,HIGH);
+      delay(100);
     }
+    
   }
 }
